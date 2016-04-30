00db6be42a36c209d7dec4651709fdbd1e2fa72b
Arvin Nguyen <arvinnguyen@ucla.edu>
2016-04-30 14:29:53 -0700

    Fix merge conflicts

--- lahacks/map/views.py
+++ lahacks/map/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from config import secret_keys
 
 # Create your views here.
@@ -8,7 +7,3 @@ from config import secret_keys
 def index(request):
     context = {}
     return render(request, 'map/index.html', context)
-=======
-
-# Create your views here.
->>>>>>> 5d2876150364bcb6767c46a9fd562eda12ba4393
