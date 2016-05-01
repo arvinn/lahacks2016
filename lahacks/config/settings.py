4d2ea7384a17883da360eb83619061441d86113b
Arvin Nguyen <arvinnguyen@ucla.edu>
2016-05-01 11:16:27 -0700

    settings

--- lahacks/config/settings.py
+++ lahacks/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(pmndj1&fgk6i)qrp24b9l-c$b+dahq7#s2ijlne18opnoj!@o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://swingbyapp.azurewebsites.net/', '*']
 
 
 # Application definition
