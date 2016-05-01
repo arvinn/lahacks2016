007b684d1a7a4c1c3b9c815ba76b15fd9a5deab9
Arvin Nguyen <arvinnguyen@ucla.edu>
2016-05-01 03:15:41 -0700

    Complete apis

--- lahacks/map/views.py
+++ lahacks/map/views.py
@@ -2,10 +2,14 @@ import json
 import requests
 import rauth
 from django.shortcuts import render
+from django.http import JsonResponse
 from config import secret_keys
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
 from math import radians, cos, sin, asin, sqrt
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 def get_line_equation(origin, destination):
     x_delta = (origin[0] - destination[0])
@@ -79,22 +83,32 @@ yelp_auth = Oauth1Authenticator(
 yelp_client = Client(yelp_auth)
 
 
-def google_search(start_lat, start_lng, end_lat, end_lng):
-    url_template = "https://maps.googleapis.com/maps/api/directions/json?origin={},{}&destination={},{}&key={}"
-    url = url_template.format(start_lat, start_lng, end_lat, end_lng, secret_keys.SERVER_GOOGLE_MAP_KEY)
+
+def google_direction_search_no_waypoints(origin_placeid, dest_placeid):
+    url_template = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:{}&destination=place_id:{}&key={}"
+    url = url_template.format(origin_placeid, dest_placeid, secret_keys.SERVER_GOOGLE_MAP_KEY)
+
+    directions = requests.get(url)
+    return directions.json()
+
+def google_direction_search(start_placeid, end_placeid, waypoint_lat, waypoint_lng):
+    url_template = "https://maps.googleapis.com/maps/api/directions/json?origin=place_id:{}&destination=place_id:{}&waypoints={},{}&key={}"
+    url = url_template.format(start_placeid, end_placeid, waypoint_lat, waypoint_lng, secret_keys.SERVER_GOOGLE_MAP_KEY)
 
     directions = requests.get(url)
     return directions.json()
 
 
 
-def yelp_search(lat,lng):
+def yelp_search(lat,lng, category=None):
+  if category is None:
+    category = "restaurants"
+
   params = {
     #"term": "restaurant",
-    "category_filter": "restaurants",
+    "category_filter": category,
     "ll": "{},{}".format(lat, lng),
     "radius_filter": "1600", #in meters
-    "limit": "10",
     "sort": "2",   # highest rated: https://www.yelp.com/developers/documentation/v2/search_api
   }
 
@@ -117,31 +131,98 @@ def index(request):
     context = {}
     return render(request, 'map/index.html', context)
 
+# TODO: fix csrf
+@csrf_exempt
 def api(request):
 
-    #x = get_n_points_in_between(9, (0,0), (10, 10))
 
+    ucla_id = 'ChIJZQ9c8IW8woARN0gTXFiTqSU'
+    hollywood_id = 'ChIJ4zPwIdm-woARpyaKDi1M5FA'
     ucla = [34.073647, -118.445141]
     hollywood = [34.090304, -118.392343]
+    usc = [34.072468, -118.360652]
     lasvegas = [36.181140, -115.132808]
 
-    directions = google_search(ucla[0], ucla[1], hollywood[0], hollywood[1])
+
+
+    origin = request.GET.get("origin", ucla_id)
+    destination = request.GET.get("destination", hollywood_id)
+    category = request.GET.get("category", "restaurants")
+    max_detour_time = int(request.GET.get("max_detour_time", 10))
+
 
     num_miles = distance(ucla, hollywood)
+
+    if num_miles > 10:
+        return JsonResponse({'status': 'ERROR: distance limit exceeded', 'places':[]},
+                json_dumps_params={'indent':'  '})
+
+
     centers = get_n_points_in_between(int(num_miles), ucla, hollywood)
 
 
     results = {}
     for center in centers:
-        businesses = yelp_search(ucla[0], ucla[1])['businesses']
+        businesses = yelp_search(ucla[0], ucla[1], category)['businesses']
         for biz in businesses:
             results[biz['id']] = biz
 
-    results = results
+    places = list(map(lambda x: x[1], results.items()))
+
+    original_directions = google_direction_search_no_waypoints(origin, destination)
+    route = original_directions['routes']
+    route = route[0]
+    legs = route['legs']
+    og_duration = 0
+    for leg in legs:
+        for step in leg['steps']:
+            d_step = step['duration']['text']
+            (n, mins) = d_step.split()
+            og_duration += int(n)
 
 
-    context = {'yelp': json.dumps(results, indent=4),
-               'google': json.dumps(directions, indent=4)
-               }
+    for place in places:
+        lat = place['location']['coordinate']['latitude']
+        lng = place['location']['coordinate']['longitude']
 
-    return render(request, 'map/api.html', context)
+        directions = google_direction_search(origin, destination, lat, lng)
+        route = directions['routes'][0]
+        legs = route['legs']
+        duration = 0
+        for leg in legs:
+            #d = leg['duration']['text']
+            #(n, mins) = d.split()
+            #duration += int(n)
+
+            for step in leg['steps']:
+                d_step = step['duration']['text']
+                (n, mins) = d_step.split()
+                duration += int(n)
+
+        place['duration'] = duration - og_duration
+
+
+       # print("=================")
+       # print(duration)
+       # print(place['name'])
+       #print(json.dumps(directions, indent=2))
+
+    places = list(filter(lambda x: x['duration'] < 5, places))
+
+
+
+
+    response = {
+        "status": "OK",
+        "count": len(places),
+        "places": places
+    }
+
+
+    #context = {'yelp': json.dumps(response, indent=4),
+    #           'google': json.dumps(directions, indent=4)
+    #           }
+    #return render(request, 'map/api.html', context)
+
+    print("=======")
+    return JsonResponse(response,  json_dumps_params={'indent':'  '})
